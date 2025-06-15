de9f33c9191515ac174156fca6df7c670b57e994
dfilterbots <ddfilterbot01@gmail.com>
2025-06-15 16:02:17 +0530

    Update info.py
    Mongodb password (<>)

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ddcinemas10:<20qiIzxn8O8swGlI>@cluster0.6ubze2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ddcinemas10:20qiIzxn8O8swGlI@cluster0.6ubze2i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
 
