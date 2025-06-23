3155814a3b1b64fb68784cf74619e047e52fc785
Muhammed Aflah <aflahtbm@gmail.com>
2025-06-23 21:13:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1934,7 +1934,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ', url="https://t.me/mrtechmoviebot")
+            InlineKeyboardButton('Bᴏᴛ Eᴅɪᴛᴇʀ', url="https://t.me/MR_BOT_EDITER")
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')
